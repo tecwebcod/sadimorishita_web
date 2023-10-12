200cd81ce2130e1115db5fd16a5c7b90f08bc659
Alexandre_oliveiradev <alexandredev.frontend@gmail.com>
2023-10-12 20:49:26 -0300

    more features

--- src/components/statistcs/index.tsx
+++ src/components/statistcs/index.tsx
@@ -4,16 +4,26 @@ import { FaClipboardList } from 'react-icons/fa'
 import { PiUsersThreeFill } from 'react-icons/pi'
 import { FaCalendarDays } from 'react-icons/fa6'
 import { useEffect, useState } from 'react'
+import ScrollRevel from 'scrollreveal'
+
 
 export default function StatistcsComponent() {
     const [soluctions, setSoluctions] = useState(0)
     const [clients, setClients] = useState(0)
     const [history, setHistory] = useState(0)
 
+
     useEffect(() => {
         let counter1 = 0;
         let counter2 = 0;
         let counter3 = 0;
+        ScrollRevel().reveal('.container-statistcs', {
+            delay: 100, reset: true, afterReset: () => {
+                counter1 = 0
+                counter2 = 0
+                counter3 = 0
+            },
+        })
 
         const interval = setInterval(() => {
             if (counter1 <= 14) {
@@ -42,30 +52,29 @@ export default function StatistcsComponent() {
             }
 
         }, 20)
-
-        console.log(soluctions)
         return () => {
             clearInterval(interval);
             clearInterval(interval2);
             clearInterval(interval3);
         };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
         <Row className='container-statistcs'>
-            <Col id='before' style={{ display: "grid", placeItems: 'center', width: "300px" }}>
+            <Col className='after' style={{ display: "grid", placeItems: 'center', width: "300px" }}>
                 <Row><FaClipboardList color='#F3941E' size={45}></FaClipboardList></Row>
                 <Row><h2 style={{ fontSize: "45px", color: "#808080" }}>+ {soluctions}</h2></Row>
                 <Row><h3 style={{ fontSize: '25px' }}>Soluções específicas</h3></Row>
             </Col>
-            <Col id='before' style={{ display: "grid", placeItems: 'center', width: "300px" }}>
+            <Col className='after' style={{ display: "grid", placeItems: 'center', width: "300px" }}>
                 <Row><PiUsersThreeFill color='#F3941E' size={45}></PiUsersThreeFill></Row>
                 <Row><h2 style={{ fontSize: "45px", color: "#808080" }}>+ {clients}</h2></Row>
                 <Row><h3 style={{ fontSize: '25px' }}>de Clientes</h3></Row>
             </Col>
-            <Col id='before' style={{ display: "grid", placeItems: 'center', width: "300px" }}>
+            <Col className='after' style={{ display: "grid", placeItems: 'center', width: "300px" }}>
                 <Row><FaCalendarDays color='#F3941E' size={45}></FaCalendarDays></Row>
-                <Row><h2 style={{ fontSize: "45px", color: "#808080" }}>+ {history}</h2></Row>
+                <Row><h2 style={{ fontSize: "45px", color: "#808080" }}>+ {history} anos</h2></Row>
                 <Row><h3 style={{ fontSize: '25px' }}>de História</h3></Row>
             </Col>
         </Row>
--- src/components/titles/index.tsx
+++ src/components/titles/index.tsx
@@ -13,7 +13,7 @@ export default function Titles({ text, direction, sizeImage, color, textExtra }:
     return (
         <div className='titles'>
             <Row style={{ flexDirection: direction, alignItems: "center" }}>
-                <div><Image style={{ width: sizeImage }} src='http://35.244.243.47/sm-barra_5.png'></Image></div>
+                <div><Image preview={false} style={{ width: sizeImage }} src='http://35.244.243.47/sm-barra_5.png'></Image></div>
                 <Row style={{ gap: "10px" }}><h3 style={{ fontSize: "40px", color: "#838383" }}>{text}</h3> <h3 style={{ fontSize: "40px", color: color ?? "#838383" }}>{textExtra}</h3></Row>
             </Row>
         </div>
